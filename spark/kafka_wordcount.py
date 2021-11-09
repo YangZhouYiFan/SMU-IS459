0840082685719540a9a5303601583ebd487032c9
YangZhouYiFan <zyfyang.2018@smu.edu.sg>
2021-11-09 16:32:12 +0800

    Revert "Revert "Revert "submit assignment 3"""
    This reverts commit 3aa36aaa4122cd43f7125edcca712f98a15e9005.

--- spark/kafka_wordcount.py
+++ spark/kafka_wordcount.py
@@ -1,18 +1,11 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
-from pyspark.sql.functions import explode, regexp_replace
+from pyspark.sql.functions import explode
 from pyspark.sql.functions import split
-from pyspark.sql.types import StringType
-from pyspark.sql.functions import udf, from_json, col, window
-from pyspark.sql.types import StringType, StructType, StructField, ArrayType
-from pyspark.sql import functions as F
-import re
-from pyspark.ml.feature import Tokenizer, StopWordsRemover
-
 
 def parse_data_from_kafka_message(sdf, schema):
     from pyspark.sql.functions import split
-    assert sdf.isStreaming == True, "DataFrame doesn't receive streaming data"
+    assert sdf.isStreaming == True, "DataFrame doesn't receive treaming data"
     col = split(sdf['value'], ',')
 
     #split attributes to nested array in one Column
@@ -21,105 +14,42 @@ def parse_data_from_kafka_message(sdf, schema):
         sdf = sdf.withColumn(field.name, col.getItem(idx).cast(field.dataType))
     return sdf
 
-def get_top_10_authors(df, epochId):
-    df \
-        .orderBy(col("window").desc(), col("count").desc()) \
-        .filter(col('author').rlike("[^ +]")) \
-        .limit(10) \
-        .show(truncate=False)
-
-def get_top_10_words(df, epochId):
-    df \
-        .orderBy(col("window").desc(), col("count").desc()) \
-        .filter(col('word').rlike("[^ +]")) \
-        .limit(10) \
-        .show(truncate=False)
-
-## data processing function to remove meaningless text in author and content columns
-def preprocessing(lines):
-    lines = regexp_replace(lines, r'[^A-Za-z\n ]|(http\S+)|(www.\S+)', '')
-    lines = regexp_replace(lines, '@\w+', '')
-    lines = regexp_replace(lines, '#', '')
-    lines = regexp_replace(lines, 'RT', '')
-    lines = regexp_replace(lines, ':', '')
-    lines = regexp_replace(lines, 'content', '')
-    lines = regexp_replace(lines, 'author', '')
-
-    return lines
-
 if __name__ == "__main__":
-    # specify the schema of the fields
+
+    spark = SparkSession.builder \
+               .appName("KafkaWordCount") \
+               .getOrCreate()
+
+    #Read from Kafka's topic scrapy-output
+    df = spark.readStream \
+            .format("kafka") \
+            .option("kafka.bootstrap.servers", "localhost:9092") \
+            .option("subscribe", "scrapy-output") \
+            .option("startingOffsets", "earliest") \
+            .load()
+
+    #Parse the fields in the value column of the message
+    lines = df.selectExpr("CAST(value AS STRING)", "timestamp")
+
+    #Specify the schema of the fields
     hardwarezoneSchema = StructType([ \
         StructField("topic", StringType()), \
         StructField("author", StringType()), \
         StructField("content", StringType()) \
         ])
 
-    # set window and trigger interval
-    window_interval = "2 minutes"
-    trigger_interval = "1 minutes"
+    #Use the function to parse the fields
+    lines = parse_data_from_kafka_message(lines, hardwarezoneSchema) \
+        .select("topic","author","content","timestamp")
 
-    # set watermark
-    watermark_time = "2 minutes"
-
-    spark = SparkSession.builder \
-               .appName("KafkaWordCount") \
-               .getOrCreate()
-
-    # from Kafka's topic scrapy-output
-    df = spark.readStream \
-            .format("kafka") \
-            .option("kafka.bootstrap.servers", "localhost:9092") \
-            .option("subscribe", "scrapy-output") \
-            .option("startingOffsets", "earliest") \
-            .load() \
-            .selectExpr("CAST(value AS STRING)", "timestamp") \
-            .dropna()
-
-    # get top 10 authors by count
-    top_authors = parse_data_from_kafka_message(df, hardwarezoneSchema) \
-        .select("author", "timestamp") \
-        .select(preprocessing("author").alias("author"), "timestamp") \
-        .withWatermark("timestamp", watermark_time) \
-        .groupBy(
-            window("timestamp", window_interval, trigger_interval), "author").count() \
+    #Select the content field and output
+    contents = lines \
         .writeStream \
-        .outputMode('append') \
-        .foreachBatch(get_top_10_authors) \
+        .queryName("WriteContent") \
+        .outputMode("append") \
+        .format("console") \
+        .option("checkpointLocation", "/user/zzj/spark-checkpoint") \
         .start()
 
-    # set stopwords remover
-    stopwords = StopWordsRemover()
-    stopwords.setInputCol("word")
-    stopwords.setOutputCol("word_cleaned")
-
-    # set tokenizer to transform content column
-    tokenizer = Tokenizer(inputCol="content_cleaned", outputCol="word")
-
-
-    lines = parse_data_from_kafka_message(df, hardwarezoneSchema) \
-            .select("content", "timestamp")
-            
-    lines_cleaned = lines.select(preprocessing("content").alias("content_cleaned"), "timestamp")
-
-    words = tokenizer.transform(lines_cleaned).select("word", "timestamp")
-    words_cleaned = stopwords.transform(words).select("word_cleaned", "timestamp")
-
-    # get top 10 words by count
-    top_words = words_cleaned \
-        .select("word_cleaned", "timestamp") \
-        .select(explode("word_cleaned").alias('word'), "timestamp") \
-        .withWatermark("timestamp", watermark_time) \
-        .groupBy(
-            window("timestamp", window_interval, trigger_interval), "word").count() \
-        .writeStream \
-        .outputMode('append') \
-        .foreachBatch(get_top_10_words) \
-        .start()
-    
-
-    top_authors.awaitTermination()
-    top_words.awaitTermination()
-    
-
-    
+    #Start the job and wait for the incoming messages
+    contents.awaitTermination()
